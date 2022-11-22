a3a9201731f53a40d61e48e80a9c2fcf2beaaf08
Nui Suriya <atchara.suriya@gmail.com>
2022-11-22 12:06:18 +0100

    Added actions && validations in products

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "products#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :products, only: [:index, :show, :new, :create ]
 end
