ee0ffeb0f12024b93faa9f419cf09e03badb36fe
nicolas rouger <nicorouger@outlook.com>
2022-11-25 15:03:44 +0100

    styling in landing page, avatar & index/cards

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,8 +4,8 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Share!"],
-      typeSpeed: 250,
+      strings: ["share!"],
+      typeSpeed: 300,
       loop: true
     })
   }
